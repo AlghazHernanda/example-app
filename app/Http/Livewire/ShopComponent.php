7baf058b7ee273abf04aacc29a2c99851f9d9bb9
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-12 18:47:30 +0700

    Initial commit

---
+++ app/Http/Livewire/ShopComponent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+use Livewire\WithPagination;
+use Gloudemans\Shoppingcart\Facades\Cart;
+
+class ShopComponent extends Component
+{
+    use WithPagination;
+
+    //untuk nyimpen pesanan ke Cart
+    public function store($product_id, $product_name, $product_price)
+    {
+        //1 = quantitiy
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
+        session()->flash('success_message', 'Item added in Cart');
+        return redirect()->route('shop.cart');
+    }
+
+    public function render()
+    {
+        $products = Product::paginate(12);
+        return view('livewire.shop-component', ['products' => $products]);
+    }
+}
